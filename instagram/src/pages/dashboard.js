a5c2a4f6a47cce58213231020f66569d8fdc1f06
siddhantranjan <sidhantranjan31@gmail.com>
2021-10-29 01:55:05 +0530

    sidebar, suggested-profiles

--- instagram/src/pages/dashboard.js
+++ instagram/src/pages/dashboard.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import Header from '../components/header';
-import Sidebar from '../components/sidebar';
+import Sidebar from '../components/sidebar/index';
 import Timeline from '../components/timiline';
 
 export default function Dashboard() {
@@ -11,9 +11,10 @@ export default function Dashboard() {
   return (
     <div className="bg-gray-background">
       <Header />
-      <div className="grid" />
-      <Timeline />
-      <Sidebar />
+      <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+        <Timeline />
+        <Sidebar />
+      </div>
     </div>
   );
 }
