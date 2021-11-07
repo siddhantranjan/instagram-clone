d663b802be4b0877e21b2ee3dbb25940cadb7367
siddhantranjan <sidhantranjan31@gmail.com>
2021-11-08 01:42:59 +0530

    post, comment, add-comment, action, footer

--- instagram/src/components/post/index.js
+++ instagram/src/components/post/index.js
@@ -2,14 +2,31 @@ import { useRef } from 'react';
 import PropTypes from 'prop-types';
 import Header from './header';
 import Image from './image';
+import Actions from './action';
+import Footer from './footer';
+import Comments from './comments';
 
 export default function Post({ content }) {
-  console.log('content', content);
+  const commentInput = useRef(null);
+  const handleFocus = () => commentInput.current.focus();
 
   return (
     <div className="rounded col-span-4 border bg-white border-gray-primary mb-12">
       <Header username={content.username} />
       <Image src={content.imageSrc} caption={content.caption} />
+      <Actions
+        docId={content.docId}
+        totalLikes={content.likes.length}
+        likedPhoto={content.userLikedPhoto}
+        handleFocus={handleFocus}
+      />
+      <Footer caption={content.caption} username={content.username} />
+      <Comments
+        docId={content.docId}
+        comments={content.comments}
+        posted={content.dateCreated}
+        commentInput={commentInput}
+      />
     </div>
   );
 }
@@ -23,6 +40,6 @@ Post.propTypes = {
     userLikedPhoto: PropTypes.bool.isRequired,
     likes: PropTypes.array.isRequired,
     comments: PropTypes.array.isRequired,
-    dateCreaeted: PropTypes.number.isRequired
+    dateCreated: PropTypes.number.isRequired
   })
 };
